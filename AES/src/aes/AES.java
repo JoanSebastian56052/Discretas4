8b371fdc99d36126f20e7a4fdb6df34c473dbb16
joansebastian56052 <chicoriolds@gmail.com>
2017-09-07 16:11:13 -0500

    AES

---
+++ AES/src/aes/AES.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aes;
+
+/**
+ *
+ * @author joan.morales
+ */
+
+public class AES {
+    public static String SBox[][] = {{"63","7C","77","7B","F2","6B", "6F","C5","30", "01", "67","2B","FE", "D7", "AB", "76"},
+        {"CA","82","C9","7D","FA","59","47", "F0", "AD", "D4", "A2","AF","9C","A4","72","C0"},
+        {""},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {}};
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
